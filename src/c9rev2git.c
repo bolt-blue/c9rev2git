625b7118c88fa231456ab04a3947bc78b246116a
bolt-blue <bluebolt@tutanota.de>
2020-09-08 15:58:08 +0400

    Add commandline options and mkdir
    - Add initial, basic options
    - Implement use of mkdir for creation of a "working directory"

--- src/c9rev2git.c
+++ src/c9rev2git.c
@@ -1,8 +1,27 @@
 /* Ref: https://www.tutorialspoint.com/sqlite/sqlite_c_cpp.htm */
 
 #include <unistd.h>     // getopt
+#include <sys/stat.h>   // mkdir
+#include <sys/types.h>  // mkdir
 #include <stdio.h>
 #include <sqlite3.h>
+#include <errno.h>
+
+/* ========================================================================== */
+
+enum OPTS
+{
+    // Must be powers of two
+    QUIET = 0x1
+};
+
+/* ========================================================================== */
+
+void print_usage()
+{
+    // TODO : Have make insert the binary name before compilation
+    fprintf(stderr, "Usage: ./c9rev2git [-q] [-o output-dir] database.db\n");
+}
 
 /*
  * data      : Data provided in the 4th argument of `sqlite3_exec()`
@@ -20,26 +39,60 @@ callback(void *data, int col_cnt, char **col_data, char **col_names)
 int
 main(int argc, char **argv)
 {
-    if (argc != 2)
+    if (argc < 2)
     {
-        fprintf(stderr, "Usage: %s [-q --quiet] [-v --verbsose] [-o output-dir] database.db\n", argv[0]);
+        print_usage();
         return 1;
     }
 
-    char *filepath = argv[1];
+    int flags, opt;
+    char *out_dir = "repo";
+
+    // Get command line args
+    while ((opt = getopt(argc, argv, "qo:")) != -1)
+    {
+        switch (opt)
+        {
+            case 'q':
+                // quiet - prevent output to stdout
+                flags |= QUIET;
+                break;
+            case 'o':
+                // Alter the output directory name
+                out_dir = optarg;
+                break;
+            default: /* '?' */
+                print_usage();
+                return 1;
+        }
+    }
+
+    // Make sure a 'database path' has been passed
+    if (optind >= argc)
+    {
+        print_usage();
+        return 1;
+    }
+
+    char *filepath = argv[optind];
     sqlite3 *db;
+
+    if ((flags & QUIET) == 0)
+    {
+        fprintf(stdout, "Opening database: %s\n", filepath);
+    }
 
     int res = sqlite3_open(filepath, &db);
 
     if (res != SQLITE_OK)
     {
+        // TODO : Utilise sqlite3_errmsg() [ref: https://www.sqlite.org/c3ref/errcode.html]
         fprintf(stderr, "Failed to open %s : %s\n", filepath, sqlite3_errmsg(db));
         return 2;
     }
 
     // TODO :
     //
-    // - Create a working directory
     // - `git init` inside that directory
     //
     // - Get a list of all the stored filenames and their id's
@@ -53,6 +106,22 @@ main(int argc, char **argv)
     //     ~ Run `git add`
     //     ~ Run `git commit` with basic message
     //
+
+    // Create working directory with permissions 755
+    if (mkdir(out_dir, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH) == -1)
+    {
+        switch (errno)
+        {
+            // TODO : Add more specific cases ?
+            case EEXIST:
+                fprintf(stderr, "[Error %d] Directory already exists. Exiting.\n", errno);
+                break;
+            default:
+                fprintf(stderr, "[Error %d] Failed to create working directory. (Ref: errno-base.h) Exiting\n", errno);
+        }
+
+        return 2;
+    }
 
     // Query to select filenames
     char file_query[255] = "SELECT id, path, FROM Documents";
