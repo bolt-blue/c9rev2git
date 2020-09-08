1218a4ecaab02ad5f57c7673ada6526c482fbf4f
bolt-blue <bluebolt@tutanota.de>
2020-09-09 01:49:03 +0400

    Add dir tree creation for targets within repo

--- src/c9rev2git.c
+++ src/c9rev2git.c
@@ -2,11 +2,13 @@
 
 #include <unistd.h>     // getopt
 
-#include <stdio.h>
 #include <errno.h>
+#include <stdio.h>      // printf, fprintf
+#include <string.h>     // strncpy
 
-#include <sys/stat.h>   // mkdir
-#include <sys/types.h>  // mkdir
+#include <sys/stat.h>   // open, mkdir
+#include <sys/types.h>  // open, mkdir
+#include <fcntl.h>      // open
 
 #include <git2.h>
 
@@ -42,18 +44,116 @@ void git2_exit_with_error(int error)
 }
 
 /*
- * data      : Data provided in the 4th argument of `sqlite3_exec()`
- * col_cnt   : The number of columns in row
- * col_data  : An array of strings representing fields in the row
- * col_names : An array of strings representing column names
-*/
+ * Process each file revision
+ *   - Update the file
+ *   - Create a new `git commit`
+ */
 static int
-callback(void *data, int col_cnt, char **col_data, char **col_names)
+process_revision_cb(void *unused, int col_cnt, char **col_data, char **col_names)
 {
+    // TODO :
+    //
+    // - For each filename:
+    //   - Open new file for writing
+    //   - Get a list of it's revision data
+    //   - For each revision:
+    //     ~ Add, Delete or Retain as necessary
+    //     ~ Save the changes to disk
+    //     ~ Run `git add`
+    //     ~ Run `git commit` with basic message
+    //
 
     return 0;
 }
 
+/*
+ * Process each target file
+ *   - Create any directory tree as required
+ *   - Create a new file
+ *   - Process the revisions
+ *
+ * Arg Reference:
+ *   data      : Data provided in the 4th argument of `sqlite3_exec()`
+ *   col_cnt   : The number of columns in row
+ *   col_data  : An array of strings representing fields in the row
+ *   col_names : An array of strings representing column names
+ *
+ * Expects:
+ *   data to be a file descriptor for the repository directory
+ *   col_data[0] to be 'id'
+ *   col_data[1] to be 'path'
+*/
+static int
+process_target_cb(void *repo_fd, int col_cnt, char **col_data, char **col_names)
+{
+    char *doc_id = col_data[0];
+    char *path   = col_data[1];
+
+    // Assuming 512 is long enough to account for most reasonable directory tree depth
+    char dir_path[512];
+
+    //   - Process 'path' and separate any directories from filename
+    int i = 0, dir_len = 0, filename_len = 0;
+
+    for (; path[i] != '\0'; i++)
+    {
+        // Create any necessary directories as we find them
+        if (path[i] == '/')
+        {
+            strncpy(dir_path, path, i);
+
+            // Always remember the null terminator
+            dir_path[i] = '\0';
+
+            // Update total directory path length
+            dir_len = i;
+
+            if (mkdirat(*(int *)repo_fd, dir_path, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH) == -1)
+            {
+                if (errno == EEXIST)
+                {
+                    // Don't worry if the directory already exists
+                    continue;
+                }
+                else
+                {
+                    // Trigger a sqlite abort
+                    fprintf(stderr, "[Error] Failed to create directory '%s'. Aborting...\n", dir_path);
+
+                    // TODO : Implement "clean up" on failure, in main(), and remove this
+                    fprintf(stderr, "[WARNING] This may leave file and/or directory artefacts.\n");
+
+                    return 1;
+                }
+            }
+        }
+    }
+
+    // This will inherently account for the null terminator
+    filename_len = i - dir_len;
+
+    // Remember to account for null terminator
+    char filename[filename_len + 1];
+    strncpy(filename, path + dir_len + 1, filename_len);
+
+    // Query to select file revision data
+    char rev_query[255];
+    sprintf(rev_query, "SELECT * FROM Revisions WHERE document_id = %s", doc_id);
+
+    printf("[DEBUG] dir: %-40s | filename: %-30s | doc_id: %5s\n", dir_path, filename, doc_id);
+
+    return 0;
+}
+
+/* ========================================================================== */
+
+/*
+ * Return codes:
+ *   0 - Success
+ *   1 - Usage error
+ *   2 - mkdir error
+ *   3 - sqlite3 error
+ */
 int
 main(int argc, char **argv)
 {
@@ -64,7 +164,7 @@ main(int argc, char **argv)
     }
 
     int flags, opt;
-    char *out_dir = "repo";
+    char *repo_dir = "repo";
 
     // Get command line args
     while ((opt = getopt(argc, argv, "qo:")) != -1)
@@ -77,7 +177,7 @@ main(int argc, char **argv)
                 break;
             case 'o':
                 // Alter the output directory name
-                out_dir = optarg;
+                repo_dir = optarg;
                 break;
             default: /* '?' */
                 print_usage();
@@ -108,7 +208,7 @@ main(int argc, char **argv)
     }
 
     // Create working directory with permissions 755
-    if (mkdir(out_dir, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH) == -1)
+    if (mkdir(repo_dir, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH) == -1)
     {
         switch (errno)
         {
@@ -123,6 +223,9 @@ main(int argc, char **argv)
         return 2;
     }
 
+    // Store the repo file descriptor
+    int repo_fd = open(repo_dir, O_DIRECTORY | O_RDONLY);
+
     // Intitialise git2 library
     git_libgit2_init();
 
@@ -134,32 +237,34 @@ main(int argc, char **argv)
         fprintf(stdout, "Initialising git repo...\n");
     }
 
-    int res = git_repository_init(&repo, out_dir, false);
+    // Temp variable for the result value of various function calls to follow
+    int res;
+
+    res = git_repository_init(&repo, repo_dir, false);
     if (res < 0)
     {
         git2_exit_with_error(res);
     }
 
-    // TODO :
-    //
-    // - Get a list of all the stored filenames and their id's
-    //
-    // - For each filename:
-    //   - Open new file for writing
-    //   - Get a list of it's revision data
-    //   - For each revision:
-    //     ~ Add, Delete or Retain as necessary
-    //     ~ Save the changes to disk
-    //     ~ Run `git add`
-    //     ~ Run `git commit` with basic message
-    //
+    char *sql_err = NULL;
 
-    // Query to select filenames
-    char file_query[255] = "SELECT id, path, FROM Documents";
+    // Extract target filenames from database
+    // Query to select filenames and id's
+    char *file_query = "SELECT id, path FROM Documents";
 
-    // Query to select file revision data
-    char rev_query[255];
-    //sprintf(rev_query, "SELECT * FROM Revisions WHERE document_id = %s", doc_id);
+    res = sqlite3_exec(db, file_query, process_target_cb, &repo_fd, &sql_err);
+    if (res != SQLITE_OK)
+    {
+        fprintf(stderr, "Failed to retrieve target filenames from database\n");
+        fprintf(stderr, "[Error: SQL] %s\n", sql_err);
+
+        sqlite3_free(sql_err);
+        sqlite3_close(db);
+
+        return 3;
+    }
+
+    // Clean up
 
     sqlite3_close(db);
 
@@ -168,6 +273,8 @@ main(int argc, char **argv)
 
     // Clean up libgit2 state (not strictly necessary)
     git_libgit2_shutdown();
+
+    close(repo_fd);
 
     return 0;
 }
